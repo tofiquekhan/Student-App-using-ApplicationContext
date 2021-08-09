7c82e8c053cad42cd64923704167e6e19f3e7366
Tofique Ahmed Khan <tofique.khan@bacancy.com>
2021-08-09 13:23:40 +0530

    Student Bean Completed

--- src/com/studentapp/beans/Student.java
+++ src/com/studentapp/beans/Student.java
@@ -2,4 +2,33 @@ package com.studentapp.beans;
 
 public class Student {
 
+	private String sid;
+	private String sname;
+	private String saddr;
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSaddr() {
+		return saddr;
+	}
+	public void setSaddr(String saddr) {
+		this.saddr = saddr;
+	}
+	
+	public void getStudentDetials() {
+		System.out.println("Student Details");
+		System.out.println("----------------------------------------");
+		System.out.println("Student Id 		: "+sid);
+		System.out.println("Student Name 	: "+sname);
+		System.out.println("Student Address : "+saddr);
+	}
 }
