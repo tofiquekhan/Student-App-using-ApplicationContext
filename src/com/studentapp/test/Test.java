6870fd48a23a4b31140a5eec85fbb2dacdd788f5
Tofique Ahmed Khan <tofique.khan@bacancy.com>
2021-08-09 13:34:50 +0530

    Test Class prepared

--- src/com/studentapp/test/Test.java
+++ src/com/studentapp/test/Test.java
@@ -1,5 +1,16 @@
 package com.studentapp.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.studentapp.beans.Student;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/studentapp/resources/applicationContext.xml");
+		Student student1 = (Student) context.getBean("student1");
+		student1.getStudentDetials();
+	}
 
 }
